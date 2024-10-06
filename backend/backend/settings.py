eb8350712abcc8a10a01c2b337ad3677c615c5fc
Dariy Topuzov <85807185+dr-programmer@users.noreply.github.com>
2024-10-06 14:26:37 +0300

    Added the ngrok domain name for accessing the local host deployed server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,9 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [\
+    '4b62-31-13-217-180.ngrok-free.app'
+]
 
 
 # Application definition
